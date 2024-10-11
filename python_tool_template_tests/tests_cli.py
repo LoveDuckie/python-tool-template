b0986ac21e41477173638531832cf96dee4e6c9c
Luc Shelton <lucshelton@gmail.com>
2024-10-11 16:43:16 +0100

    Optimizing imports, adding documentation

--- python_tool_template_tests/tests_cli.py
+++ python_tool_template_tests/tests_cli.py
@@ -1,12 +1,15 @@
 import unittest
 from unittest.mock import patch
 
-import click
 from click.testing import CliRunner
+
 from python_tool_template.__main__ import cli
 
 
 class TestsCli(unittest.TestCase):
+    """
+    Unit tests to run against the command-line interface of the tool.
+    """
 
     def setUp(self):
         self.runner = CliRunner()
